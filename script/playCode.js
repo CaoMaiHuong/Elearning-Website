1af8936fb39700f6c5fea24e84c57e6c6d55f904
huongcm1 <huongcm1@smartosc.com>
2019-11-24 15:23:59 +0700

    update code

--- script/playCode.js
+++ script/playCode.js
@@ -2,4 +2,15 @@ $( document ).ready(function() {
     $('.item__title-icon').click(function(){        
         $(this).parent().siblings().find('.input-add').toggleClass('active');
     });
+    $('.sublist-item__name').dblclick(function(){  
+        var currentName = $(this).html().trim();  
+        $(this).replaceWith("<div class='sublist-item__name'><input type='text' name = 'name' value='"+currentName+"'></div>" );
+    //    console.log($(this).html());
+            
+        // $(this).html().replace("dfdfdf")
+        // <input type='text' name = 'name' value='"+currentName+">";
+    });
+    
+    // document.getElementById("demo").innerHTML = "Hello World";
+
 });
